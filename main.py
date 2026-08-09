7b9e7f1055e6068b2b63269a6732e133bda6bb3c
Ricsmokey <kareemakoredeuthman@gmail.com>
2026-08-09 11:44:32 +0100

    CRUD API TASKS

---
+++ main.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI(title ="Todo List API")
+@app.get("/")
+async def root():
+    return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+
+# Pydantic models
+class CreateTask(BaseModel):
+    title: str
+
+class Task(BaseModel):
+    title: str
+
+class UpdateTask(BaseModel):
+    title: Optional[str] = None
+    done: Optional[bool] = None
+    
+
+db = {
+    1: {"id": 1, "title": "Task 1", "done": False},
+    2: {"id": 2, "title": "Task 2", "done": True},
+    3: {"id": 3, "title": "Task 3", "done": False}
+}
+
+# CRUD models
+# Get tasks
+@app.get("/tasks")
+def get_tasks():
+    return list(db.values())
+
+# Get task by ID
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    task = db.get(task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail={"error": f"Task {task_id} not found"})
+    return task
+
+
+
+# Create a new task
+@app.post("/tasks", status_code=201)
+def create_task(task: CreateTask):
+    if not task.title.strip():
+        raise HTTPException(status_code=400, detail={"error": "Bad request"})
+    new_id = max(db.keys()) + 1 if db else 1
+    db[new_id] = {"id": new_id, "title": task.title, "done": False}
+    return db[new_id]
+
+
+# Update and Delete
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task: UpdateTask):
+    if task_id not in db:
+        raise HTTPException(status_code=404, detail={"error": f"Task {task_id} not found"})
+    
+    if task.title is not None:
+        db[task_id]["title"] = task.title
+    if task.done is not None:
+        db[task_id]["done"] = task.done
+    return db[task_id]
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    if task_id not in db:
+        raise HTTPException(status_code=404)
+    db.pop(task_id)
+
+    
